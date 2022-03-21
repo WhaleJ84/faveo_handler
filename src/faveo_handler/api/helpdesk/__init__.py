c9ad4a0497c5dc782e838438a29c876d2b3c21bb
James Whale <jamwhale@cisco.com>
2022-03-21 13:58:48 +0000

    inherited Delete class

--- src/faveo_handler/api/helpdesk/__init__.py
+++ src/faveo_handler/api/helpdesk/__init__.py
@@ -1,7 +1,8 @@
 from .create import Create
 from .read import Read
+from .delete import Delete
 
 
-class Helpdesk(Create, Read):
+class Helpdesk(Create, Read, Delete):
     def __init__(self, url: str):
         super().__init__(url)

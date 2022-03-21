126b7fcee19231cc1cf6bf850f0ff6c95ad841bd
James Whale <jamwhale@cisco.com>
2022-03-21 15:21:35 +0000

    included update endpoint

--- src/faveo_handler/api/helpdesk/__init__.py
+++ src/faveo_handler/api/helpdesk/__init__.py
@@ -1,8 +1,9 @@
 from .create import Create
 from .read import Read
+from .update import Update
 from .delete import Delete
 
 
-class Helpdesk(Create, Read, Delete):
+class Helpdesk(Create, Read, Update, Delete):
     def __init__(self, url: str):
         super().__init__(url)
